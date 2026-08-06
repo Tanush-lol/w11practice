5f91bbc1b899d804f9bfec77f8d4155dd58433d2
Tanush-lol <tanushroy1@gmail.com>
2026-08-06 17:38:45 +0000

    haha

--- waytask.c
+++ waytask.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
-#include <xdg-shell-protocol.h>
 #include <wayland-client.h>
+#include <xdg-shell-protocol.h>
 
 static struct wl_compositor *compositor = NULL;
 static struct wl_shm *shm = NULL;
 static struct xdg_wm_base *wm_base = NULL;
-
+struct xdg_surface *surface_xdg;
 static struct wl_surface *surface;
 
-static void sendPing(void *data,struct xdg_wm_base *base,uint32_t serial){
-  xdg_wm_base_pong(base,serial);
+static void sendPing(void *data, struct xdg_wm_base *base, uint32_t serial) {
+  xdg_wm_base_pong(base, serial);
 }
 
 static struct xdg_wm_base_add_listener wm_base_listener = {
-  .ping = sendPing,
+    .ping = sendPing,
 };
 
-static void registry_add(void *data, struct wl_registry *registry,uint32_t name, const char *interface,uint32_t version){
-  if (strcmp(interface,wl_compositor_interface.name) == 0) {
-    compositor = wl_registry_bind(registry,id,&wl_compositor_interface,4);
-  }else if (strcmp(interface,wl_shm_interface.name) == 0) {
-    shm = wl_registry_bind(registry,id,&wl_shm_interface,1);
-  }else if (strcmp(interface,&xdg_wm_base_interface.name) == 0) {
-    xdg_wm_base = wl_registry_bind(interface,&xdg_wm_base_interface,1);
-    xdg_wm_base_add_listener(wm_base,wm_base_listener,NULL);
+static void registry_add(void *data, struct wl_registry *registry,
+                         uint32_t name, const char *interface,
+                         uint32_t version) {
+  if (strcmp(interface, wl_compositor_interface.name) == 0) {
+    compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 4);
+  } else if (strcmp(interface, wl_shm_interface.name) == 0) {
+    shm = wl_registry_bind(registry, id, &wl_shm_interface, 1);
+  } else if (strcmp(interface, &xdg_wm_base_interface.name) == 0) {
+    xdg_wm_base = wl_registry_bind(interface, &xdg_wm_base_interface, 1);
+    xdg_wm_base_add_listener(wm_base, wm_base_listener, NULL);
   }
 }
 
-static void registry_remove(void *data, struct wl_registry *registry,uint32_t name, const char *interface,uint32_t version){
+static void registry_remove(void *data, struct wl_registry *registry,
+                            uint32_t name, const char *interface,
+                            uint32_t version) {}
 
+static void xdg_surface_add(void *data, struct xdg_surface *surface) {
+  xdg_surface_ack_configure(surface, serial);
 }
 
 static const struct wl_registry_listener reglistener = {
@@ -42,16 +48,16 @@ int main(){
     fprintf(stderr, "failed to connect to the wayland display server \n");
   }
   struct wl_registry *registry = wl_display_get_registry(display);
-  wl_display_add_listener(registry,&reglistener,NULL);
+  wl_display_add_listener(registry, &reglistener, NULL);
 
   wl_display_roundtrip(display);
-  if(!compositor || !wm_base || !shm){
+  if (!compositor || !wm_base || !shm) {
     fprintf(stderr, "waytask failed to initialize\n");
     return 1;
   }
 
   struct wl_surface *surface = wl_compositor_create_surface(compositor);
-  xdg_surface = 
+  *surface_xdg = xdg_wm_base_get_xdg_surface(wm_base, surface);
 
   printf("Hello Wrld\n");
 }
