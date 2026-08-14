0cd2b9115c9e7a9d2a0b5dbf105073a55f91205a
Tanush-lol <tanushroy1@gmail.com>
2026-08-14 04:49:29 +0000

    new shi

--- w.c
+++ w.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
-#inlcude "wayland.h"
+#include <wayland-client.h>
+#include "wayland-protocol.h"
+
+int main(){
+  struct wl_display *display = wl_display_connect("wayland-1");
+  if (!display) {
+    fprintf(stderr, "we couldn't get the display on wayland server\n");
+    return 1;
+  }
+  printf("connection established");
+  while (wl_display_dispatch(display) != -1) {
+    printf("Doing smthng");
+  }
+
+  int wl_display_get_fd(struct wl_display *display); // to get the file descriptor  
+
+  wl_display_disconnect(display);
+  return 0;
+}
